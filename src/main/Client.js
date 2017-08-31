f7c739341a3c0fd2f5498bfcbc7397a4d01736eb
AnounFX <pertoli@mail.com>
2017-08-31 21:17:58 +0100

    owners

--- src/main/Client.js
+++ src/main/Client.js
@@ -3,7 +3,8 @@ const { Client } = require('discord.js');
 /**
  * Set these options to choose on how the framework works
  * @typedef {Object} Options
- * @prop {boolean} [selfbot=false] Say if its a selfbot or not
+ * @prop {boolean} [selfbot=false] Say if its a selfbot or not.
+ * @prop {string|string[]} [ownersID=''] The ID of the owners.
  */
 
  class AnonClient extends Client {
@@ -19,6 +20,12 @@ const { Client } = require('discord.js');
            * Choose if this is a selfbot or not.
            * @type {boolean}
            */
-          this.selfbot = !!options.selfbot
+          this.selfbot = !!options.selfbot;
+
+          /**
+           * ID(s) of the owner(s)
+           * @type {string}
+           */
+          this.ownersID = options.ownersID;
       }
  }
